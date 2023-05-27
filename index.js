597e3bf66f982fa479a6c0f54f7ea59e41735aae
EuryJesus <Eury0218@hotmail.com>
2023-05-26 20:57:52 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(msg = 'roller-skate'){
+    return `This Saturday, I want to ${msg}!`
+}
+
+function mondayWork(msg = 'go to the office'){
+    return `This Monday, I will ${msg}.`
+}
+
+function wrapAdjective(special){
+    const stage1 = 'You are...!';
+return  function (msg = 'a hard worker'){
+    const stage2 = 'You are...!';
+    return function(msg = 'a dedicated programmer'){
+        console.log(`${stage1} ${stage2} ${special}`)
+
+      };
+    };
+ }
+ const encouragingPromptFucntion = wrapAdjective('special')
+wrapAdjective('special')('a dedicated programmer');
+wrapAdjective('special')('a hard worker')
